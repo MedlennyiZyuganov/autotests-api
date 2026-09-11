e64e0d59dbb5f6ed043cb3f4de06d8ab9fda1ba2
MedlennyiZyuganov <osilovb@bk.ru>
2026-09-11 07:38:35 +0300

    Введение в HTTPX

--- httpx_example.py
+++ httpx_example.py
@@ -1,61 +1,61 @@
 import httpx
 
-# response = httpx.get('https://jsonplaceholder.typicode.com/todos/1')
-#
-# print(response.status_code)
-# print(response.json())
-#
-#
-# data = {
-#     "title": "Новая задача",
-#     "completed": False,
-#     "userId": 1
-# }
-# response = httpx.post('https://jsonplaceholder.typicode.com/todos', json=data)
-#
-# print(response.status_code)
-# print(response.request.headers)
-# print(response.json())
-#
-# data = {"username": "test_user", "password": "123456"}
-# response = httpx.post('https://httpbin.org/post', data=data)
-#
-# print(response.status_code)
-# print(response.request.headers)
-# print(response.json())
-#
-# headers = {"Authorization": "Bearer my_secret_token"}
-# response = httpx.get('https://httpbin.org/get', headers=headers)
-# print(response.request.headers)
-# print(response.json())
-#
-# response = httpx.get("https://jsonplaceholder.typicode.com/todos?UserId=1", headers=headers)
-#
-# print(response.url)
-# print(response.json())
-#
-# params = {"UserId": 1}
-# response = httpx.post('https://jsonplaceholder.typicode.com/todos', params=params)
-#
-# print(response.url)
-# print(response.json())
-#
-# files = {"file": ("example.txt", open("example.txt", "rb"))}
-# response = httpx.post('https://httpbin.org/post', files=files)
-#
-# print(response.json())
-#
-# with httpx.Client() as client:
-#     response1 = client.get('https://jsonplaceholder.typicode.com/todos/1')
-#     response2 = client.get('https://jsonplaceholder.typicode.com/todos/2')
-#
-# print(response1.json())
-# print(response2.json())
-#
-# httpx.Client(headers={"Authorization": "Bearer my_secret_token"})
-# response = httpx.get('https://httpbin.org/get')
-#
-# print(response.json())
+response = httpx.get('https://jsonplaceholder.typicode.com/todos/1')
+
+print(response.status_code)
+print(response.json())
+
+
+data = {
+    "title": "Новая задача",
+    "completed": False,
+    "userId": 1
+}
+response = httpx.post('https://jsonplaceholder.typicode.com/todos', json=data)
+
+print(response.status_code)
+print(response.request.headers)
+print(response.json())
+
+data = {"username": "test_user", "password": "123456"}
+response = httpx.post('https://httpbin.org/post', data=data)
+
+print(response.status_code)
+print(response.request.headers)
+print(response.json())
+
+headers = {"Authorization": "Bearer my_secret_token"}
+response = httpx.get('https://httpbin.org/get', headers=headers)
+print(response.request.headers)
+print(response.json())
+
+response = httpx.get("https://jsonplaceholder.typicode.com/todos?UserId=1", headers=headers)
+
+print(response.url)
+print(response.json())
+
+params = {"UserId": 1}
+response = httpx.post('https://jsonplaceholder.typicode.com/todos', params=params)
+
+print(response.url)
+print(response.json())
+
+files = {"file": ("example.txt", open("example.txt", "rb"))}
+response = httpx.post('https://httpbin.org/post', files=files)
+
+print(response.json())
+
+with httpx.Client() as client:
+    response1 = client.get('https://jsonplaceholder.typicode.com/todos/1')
+    response2 = client.get('https://jsonplaceholder.typicode.com/todos/2')
+
+print(response1.json())
+print(response2.json())
+
+httpx.Client(headers={"Authorization": "Bearer my_secret_token"})
+response = httpx.get('https://httpbin.org/get')
+
+print(response.json())
 
 try:
     response = httpx.get('https://jsonplaceholder.typicode.com/invalid_url')
@@ -65,5 +65,6 @@ except httpx.HTTPStatusError as e:
 
 try:
     response = httpx.get('https://httpbin.org/delay/5', timeout=2)
-except httpx.ReadTimeout:
+except httpx.Timeout:
     print("Запрос превысил лимит времени")
+    
